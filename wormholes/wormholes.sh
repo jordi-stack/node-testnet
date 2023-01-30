0607a16a51b95790691242003384d20ea4a3a872
Dwentzart <118625308+Dwentzart@users.noreply.github.com>
2023-01-31 04:15:38 +0700

    Create wormholes.sh

---
+++ wormholes/wormholes.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+. ~/.bashrc
+   
+    mkdir -p .wormholes/wormholes
+    
+    sleep 5
+    
+if [ ! $PRIVATE_KEY ]; then
+	read -p "enter privatekey: " PRIVATE_KEY
+	echo $PRIVATE_KEY>> $HOME/.wormholes/wormholes/nodekey
+	source $HOME/.bash_profile
+fi
+
+#update package
+sudo apt update && sudo apt list --upgradable && sudo apt upgrade -y
+
+#install library
+sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git make ncdu net-tools -y
+
+#install go
+ver="1.19" && \
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
+sudo rm -rf /usr/local/go && \
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
+rm "go$ver.linux-amd64.tar.gz" && \
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
+source $HOME/.bash_profile && \
+go version
+
+#install binary
+cd $HOME
+git clone https://github.com/wormholes-org/wormholes
+cd wormholes
+make wormholes
+cp ./build/bin/wormholes  /usr/local/bin
+
+#create service
+tee /etc/systemd/system/wormholesd.service > /dev/null <<EOF
+[Unit]
+Description=wormholes
+After=network.target
+[Service]
+Type=simple
+User=$USER
+ExecStart= /usr/local/bin/wormholes\
+  --datadir $HOME/.wormholes \
+  --devnet \
+  --mine \
+  --miner.threads 2 \
+  --rpc \
+  --rpccorsdomain "*" \
+  --rpcvhosts "*" \
+  --http \
+  --rpcaddr 127.0.0.1 \
+  --rpcport 8545 \
+  --port 30300 \
+  --maxpeers 50 \
+  --syncmode full
+Restart=on-failure
+RestartSec=5
+LimitNOFILE=4096
+[Install]
+WantedBy=multi-user.target
+EOF
+
+#start service
+sudo systemctl daemon-reload
+sudo systemctl enable wormholesd
+sudo systemctl restart wormholesd
+
+sleep 10
+NODE_KEY=$(cat $HOME/.wormholes/wormholes/nodekey)
+echo -e "Your privatekey: \e[32m$NODE_KEY\e[39m"
